946109f1e06529aaf6418e825183436c64df0748
ywssp <ywspcodes@gmail.com>
2024-05-02 16:39:59 +0800

    Allow automatic switching between Normal and High Contrast Colors

--- ColoredSuitTarots.lua
+++ ColoredSuitTarots.lua
@@ -2,7 +2,7 @@
 --- MOD_NAME: Colored Suit Tarots
 --- MOD_ID: ColoredSuitTarots
 --- MOD_AUTHOR: [ywssp]
---- MOD_DESCRIPTION: Colors the suit-changing tarot cards to their respective suits. Based on the high contrast color scheme.
+--- MOD_DESCRIPTION: Colors the suit-changing tarot cards to their respective suits.
 --- PRIORITY: 1
 
 ----------------------------------------------
@@ -13,7 +13,22 @@ function SMODS.INIT.ColoredSuitTarots()
 
     local color_mod = SMODS.findModByID("ColoredSuitTarots")
 
-    local sprite_tarot = SMODS.Sprite:new('Tarot', color_mod.path, 'Tarots.png', 71, 95, 'asset_atli')
+    local isHighContrast = G.SETTINGS.colourblind_option
+
+    local normalFilePath = 'Tarots.png'
+    local highContrastFilePath = 'Tarots_HighContrast.png'
+
+    local filePath = ''
+
+    if (isHighContrast) then
+        sendDebugMessage("High Contrast Setting Turned On, Using file: " .. highContrastFilePath)
+        filePath = highContrastFilePath
+    else
+        sendDebugMessage("High Contrast Setting Turned Off, Using file: " .. normalFilePath)
+        filePath = normalFilePath
+    end
+
+    local sprite_tarot = SMODS.Sprite:new('Tarot', color_mod.path, filePath, 71, 95, 'asset_atli')
 
     sprite_tarot:register()
 end
